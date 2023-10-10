d0443d0134e3c29d0f67386a4a4e9596074cd2d7
andrevpc <andrevictor06@gmail.com>
2023-10-10 14:20:40 -0300

    aula 1 de funcoes

---
+++ aula1_funcoes.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+fruta = 'banana'
+letra = fruta[0]
+
+#funcao len - le a quantidade de caracteres
+print(len(fruta))
+
+#usar \ para usar caracteres que tem comandos em uma frase ex: 'raven\'s'
+#\\ printar o caractere \ sem quebrar linha
+
+#funcao join - junta as palavras com um simbolo
+print('-' .join(['palavra','sim']))
+
+#funcao split
+s = "n o m-e"
+print(s.split(' '))
+
+#funcao lower - letra minuscula
+#funcao upper - letra maiuscula
+
+s = "Python"
+print(s.upper())
+print(s.lower())
+
+
